c189d62b48bb5426df3259344a8954dbb500a729
Zongchang Yang <yangzch@gmail.com>
2026-04-29 10:52:55 +0200

    new max zoom level, 18 for all layers

--- app.py
+++ app.py
@@ -683,18 +683,18 @@ var map = L.map('map', {
 var basemaps = {
     esri: L.tileLayer(
         'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-        {attribution:'Tiles &copy; Esri', maxZoom:20, maxNativeZoom:18}
+        {attribution:'Tiles &copy; Esri', maxZoom:18}
     ),
     osm: L.tileLayer(
         'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-        {attribution:'&copy; OpenStreetMap contributors', maxZoom:20, maxNativeZoom:19}
+        {attribution:'&copy; OpenStreetMap contributors', maxZoom:18}
     ),
     google: L.tileLayer(
         'https://mt{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',
         {
             attribution:'Imagery &copy; Google, Airbus, Maxar Technologies',
             subdomains:['0','1','2','3'],
-            maxZoom:20
+            maxZoom:18
         }
     )
 };
@@ -772,7 +772,7 @@ function applyLayer(datasetKey, year, layerId, tileUrl){
 
     var layer = L.tileLayer(tileUrl, {
         opacity: currentOpacity,
-        maxZoom: 20,
+        maxZoom: 18,
         crossOrigin: true
     });
     layer.addTo(map);
